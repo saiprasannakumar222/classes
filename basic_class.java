24fb8e0c6dbfc29489c36d78b40dbe98c24a06ba
sai prasana kumar <saiprasannakumar222@gmail.com>
2020-10-28 13:15:05 +0530

    Initial commit

---
+++ basic_class.java
@@ -0,0 +1,25 @@
+class basic_class{
+public static void main(String[] args){
+basic_class b = new basic_class();
+b.method1();  
+}
+void method1(){
+System.out.println("basic class method 1");
+
+}
+}
+class basic_class_2{
+ void method2(){
+System.out.println("Basic class_2 method_2");
+}
+}
+class basic_class_3{
+ void method3(){
+System.out.println("Basic class_3 method_3");
+}
+}
+class basic_class_4{
+ void method4(){
+System.out.println("Basic class_4 method_4");
+}
+}
